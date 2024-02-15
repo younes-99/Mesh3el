2758bee387c0c6032239b8d974b8c25677055b75
Younes Elkhatib <younes.elkhatib@paymentsense.com>
2024-02-15 23:23:53 +0000

    adding the echo bot

--- Mesh3el.py
+++ Mesh3el.py
@@ -2,9 +2,10 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 st.title('Mesh3el Chatbot')
 
-st.chat_input('Type a message')
 
 # Function to read data from SQLite database
 def read_from_db():
@@ -18,3 +19,28 @@ def read_from_db():
 st.write('Here is the data from the SQLite database:')
 t = read_from_db()
 st.dataframe(pd.DataFrame(t))
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
